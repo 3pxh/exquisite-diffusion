fb2822fc3373ab5b473af076c05643c8a2307a07
George H <george@hoqqanen.com>
2022-12-10 18:41:29 -0800

    PromptsOnly version

--- dev/src/index.tsx
+++ dev/src/index.tsx
@@ -2,6 +2,6 @@
 import { render } from 'solid-js/web'
 
 import './index.css'
-import App from './App'
+import App from './PromptsOnly'
 
 render(() => <App />, document.getElementById('root') as HTMLElement)
