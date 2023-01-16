4c98f5846f28a9ad67e21281ac287dcff4721fb1
Marcello Herreshoff <marcello.herreshoff@gmail.com>
2023-01-16 14:00:51 -0800

    Broken version of chat colors.

--- dev/src/Chatroom.tsx
+++ dev/src/Chatroom.tsx
@@ -7,11 +7,30 @@ interface Message {
   text: string,
 }
 
+const HTML_PLAYER_COLORS = [
+    "#ff0000", // red
+    "#0000ff", // blue
+    "#00ff00", // green
+    "#ff7000", // orange (that's right!)
+    "#ffff70", // yellow
+    "#ff00ff", // purple
+]
+
+const getHtmlPlayerStyle = index => {
+    if (index == -1) {
+        return "color:#ffffff" // Use white for unknown player/error
+    }
+    return "color:" + HTML_PLAYER_COLORS[index % HTML_PLAYER_COLORS.length];
+}
+
 const Chatroom: Component<{roomId: number}> = (props) => {
   const { session, playerHandle } = useAuth();
 
   const [messages, setMessages] = createSignal<Message[]>([])
+  const [uuidToPlayerIndex, setUuidToPlayerIndex] = createSignal<{ [key: string]: number }>({})
   const [isOpen, setIsOpen] = createSignal<boolean>(true)
+
+  var uuid_to_room_index = {};
 
   createEffect(async () => {
     const el = document.getElementById("Chatroom-Messages");
@@ -29,6 +48,27 @@ const Chatroom: Component<{roomId: number}> = (props) => {
           el.scrollTop = el.scrollHeight;
         }
       }).subscribe();
+
+    const updateUuidTable = async payload => {
+        const participants = await supabase.from('participants').select(`user`).eq('id', props.roomId).data;
+        console.log(`Got participants ${participants}`)
+        const new_uuid_to_player_index : { [key: string]: int} = {};
+        for (var i = 0; i < participants.length; i++) {
+            console.log(`Player ${i} is ${participants[i].user}`)
+            new_uuid_to_player_index[participants[i].user] = i;
+        }
+        setUuidToPlayerIndex(new_uuid_to_player_index);
+    };
+    updateUuidTable();
+
+    supabase
+      .channel(`public:participants:room_id=eq.${props.roomID}`)
+      .on('postgress_changes', {
+        event: 'INSERT', schema: 'public', table: 'chats', filter: `room_id=eq.${props.roomId}` 
+        // TODO: we also want this for delete.
+        // Also, this solution is imperfect because if a player gets deleted the colors of subsequent players
+        // rotate back.
+      }, updateUuidTable).subscribe();
   })
 
   const sendMessage = async () => {
@@ -58,7 +98,10 @@ const Chatroom: Component<{roomId: number}> = (props) => {
       {isOpen() ? <>
         <div id="Chatroom-Messages">
           <For each={messages()}>{(m, i) =>
-              <p><strong>{m.player}:</strong> {m.text}</p>
+            <p>
+              <strong style={getHtmlPlayerStyle(uuidToPlayerIndex()[m.player.uuid] ?? -1)} > {m.player}:</strong>
+              {m.text}
+            </p>
           }</For>
         </div> 
         <div id="Chatroom-Input">
