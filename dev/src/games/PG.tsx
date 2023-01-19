28a9a0676d434c01dfa8266e71cfcab6cf817442
George H <george@hoqqanen.com>
2023-01-19 13:48:11 -0800

    Color fixes

--- dev/src/games/PG.tsx
+++ dev/src/games/PG.tsx
@@ -81,104 +81,106 @@ const PG: Component<Room & {engine: PromptGuessGameEngine}> = (props) => {
       <Show when={playerState() !== State.Lobby}>
         <div class="PG-timer" style={`width: ${props.engine.timer.percentRemaining() > 0 ? props.engine.timer.percentRemaining() : 100}%`}></div>
         <div class="PG-Game">
-          <div class="PG-Game-Left">
-          <div class="PG-Game-Left-Prompt">
-              <Switch fallback={"Unrecognized Game State"}>
-                <Match when={playerState() === State.WritingPrompts}>
-                  {props.engine.renderPrompt()}
-                  <div style="margin-top: 10px;">
-                    <input onchange={(e) => { setInputVal(e.currentTarget.value) }} />
-                    <button onclick={() => props.engine.generate(inputVal())}>Make it so!</button>
-                  </div>
-                </Match>
-                <Match when={playerState() === State.CreatingLies}>
-                  <Show when={props.engine.gameState.generations.length > 0}
-                    fallback={"Waiting on the AI..."}>
-                    <Show when={props.engine.gameState.generations[0].player.id !== props.userId}
-                      fallback={"You are responsible for this masterpiece. gj."}>
-                      {props.engine.renderGenerationPrompt(props.engine.gameState.generations[0])}
-                      <div style="margin-top: 10px;">
-                        <input onchange={(e) => { setInputVal(e.currentTarget.value) }} />
-                        <button onclick={() => props.engine.caption(inputVal())}>Fool others!</button>
-                      </div>
+          <div class="PG-Game-Container">
+            <div class="PG-Game-Left">
+              <div class="PG-Game-Left-Prompt">
+                <Switch fallback={"Unrecognized Game State"}>
+                  <Match when={playerState() === State.WritingPrompts}>
+                    {props.engine.renderPrompt()}
+                    <div style="margin-top: 10px;">
+                      <input onchange={(e) => { setInputVal(e.currentTarget.value) }} />
+                      <button onclick={() => props.engine.generate(inputVal())}>Make it so!</button>
+                    </div>
+                  </Match>
+                  <Match when={playerState() === State.CreatingLies}>
+                    <Show when={props.engine.gameState.generations.length > 0}
+                      fallback={"Waiting on the AI..."}>
+                      <Show when={props.engine.gameState.generations[0].player.id !== props.userId}
+                        fallback={"You are responsible for this masterpiece. gj."}>
+                        {props.engine.renderGenerationPrompt(props.engine.gameState.generations[0])}
+                        <div style="margin-top: 10px;">
+                          <input onchange={(e) => { setInputVal(e.currentTarget.value) }} />
+                          <button onclick={() => props.engine.caption(inputVal())}>Fool others!</button>
+                        </div>
+                      </Show>
                     </Show>
-                  </Show>
-                </Match>
-                <Match when={playerState() === State.Waiting}>
-                  Waiting on other players.
-                </Match>
+                  </Match>
+                  <Match when={playerState() === State.Waiting}>
+                    Waiting on other players.
+                  </Match>
+                  <Match when={playerState() === State.Voting}>
+                    {props.engine.renderGenerationPrompt(props.engine.gameState.generations[0])}
+                  </Match>
+                  <Match when={playerState() === State.Scoring}>
+                    <>
+                      {/* <h3>Who got it right?</h3> */}
+                      The truth was: <strong>{props.engine.gameState.generations[0].prompt}</strong>
+                    </>
+                  </Match>
+                </Switch>
+                <p class="PG-Game-Error">{props.engine.error()}</p>
+              </div>
+              <div class="PG-Game-Left-Generation">
+                <Switch>
+                  <Match when={playerState() === State.WritingPrompts}>
+                    <h2>Write a prompt above!</h2>
+                    {/* Swap with a custom message per game */}
+                  </Match>
+                  <Match when={playerState() === State.CreatingLies || 
+                              playerState() === State.Voting ||
+                              playerState() === State.Scoring}>
+                    <Show when={props.engine.gameState.generations.length > 0} fallback={"Waiting on the AI..."}>
+                      {props.engine.renderGeneration(props.engine.gameState.generations[0])}
+                    </Show>
+                  </Match>
+                </Switch>
+              </div>
+            </div>
+            <div class="PG-Game-Right">
+              <Show when={playerState() !== State.Scoring}>
+                <div class="PG-WaitingPlayers">
+                  <For each={props.engine.players()}>{(p, i) => {
+                    const gens = props.engine.gameState.generations;
+                    return <>
+                      <img class={p.state === State.Waiting || (gens.length > 0 && gens[0].player.id === p.id)
+                        ? "PG-WaitingPlayers--Done" : "PG-WaitingPlayers--NotDone"} 
+                          src={p.avatar} />
+                    </>
+                  }}</For>
+                </div>
+              </Show>
+              <Switch>
                 <Match when={playerState() === State.Voting}>
-                  {props.engine.renderGenerationPrompt(props.engine.gameState.generations[0])}
+                  <ol>
+                    <For each={props.engine.gameState.captions}>{(c, i) =>
+                      <li>
+                        <Switch>
+                          <Match when={props.engine.gameState.generations[0].player.id === props.userId}>
+                          <h4>{c.caption}</h4>
+                          </Match>
+                          <Match when={c.player === props.userId}>
+                            <button style="opacity:.5;">{c.caption}</button>
+                          </Match>
+                          <Match when={true}>
+                            <button onclick={() => {props.engine.vote(c.player)}}>{c.caption}</button>
+                          </Match>
+                        </Switch>
+                      </li>
+                    }</For>
+                  </ol> 
                 </Match>
                 <Match when={playerState() === State.Scoring}>
-                  <>
-                    {/* <h3>Who got it right?</h3> */}
-                    The truth was: <strong>{props.engine.gameState.generations[0].prompt}</strong>
-                  </>
-                </Match>
-              </Switch>
-              <p class="PG-Game-Error">{props.engine.error()}</p>
-            </div>
-            <div class="PG-Game-Left-Generation">
-              <Switch>
-                <Match when={playerState() === State.WritingPrompts}>
-                  <h2>Write a prompt above!</h2>
-                  {/* Swap with a custom message per game */}
-                </Match>
-                <Match when={playerState() === State.CreatingLies || 
-                            playerState() === State.Voting ||
-                            playerState() === State.Scoring}>
-                  <Show when={props.engine.gameState.generations.length > 0} fallback={"Waiting on the AI..."}>
-                    {props.engine.renderGeneration(props.engine.gameState.generations[0])}
+                  <Scoreboard players={props.engine.players()} scores={props.engine.gameState.scores} />
+                  <Show when={props.isHost}>
+                    <button onclick={() => { props.engine.continueAfterScoring() }}>Continue</button>
                   </Show>
                 </Match>
+                <Match when={playerState() === State.Finished}>
+                  <h2>Final Scores:</h2>
+                  <Scoreboard players={props.engine.players()} scores={props.engine.gameState.scores} />
+                </Match>
               </Switch>
             </div>
-          </div>
-          <div class="PG-Game-Right">
-            <Show when={playerState() !== State.Scoring}>
-              <div class="PG-WaitingPlayers">
-                <For each={props.engine.players()}>{(p, i) => {
-                  const gens = props.engine.gameState.generations;
-                  return <>
-                    <img class={p.state === State.Waiting || (gens.length > 0 && gens[0].player.id === p.id)
-                      ? "PG-WaitingPlayers--Done" : "PG-WaitingPlayers--NotDone"} 
-                        src={p.avatar} />
-                  </>
-                }}</For>
-              </div>
-            </Show>
-            <Switch>
-              <Match when={playerState() === State.Voting}>
-                <ol>
-                  <For each={props.engine.gameState.captions}>{(c, i) =>
-                    <li>
-                      <Switch>
-                        <Match when={props.engine.gameState.generations[0].player.id === props.userId}>
-                        <h4>{c.caption}</h4>
-                        </Match>
-                        <Match when={c.player === props.userId}>
-                          <button style="opacity:.5;">{c.caption}</button>
-                        </Match>
-                        <Match when={true}>
-                          <button onclick={() => {props.engine.vote(c.player)}}>{c.caption}</button>
-                        </Match>
-                      </Switch>
-                    </li>
-                  }</For>
-                </ol> 
-              </Match>
-              <Match when={playerState() === State.Scoring}>
-                <Scoreboard players={props.engine.players()} scores={props.engine.gameState.scores} />
-                <Show when={props.isHost}>
-                  <button onclick={() => { props.engine.continueAfterScoring() }}>Continue</button>
-                </Show>
-              </Match>
-              <Match when={playerState() === State.Finished}>
-                <h2>Final Scores:</h2>
-                <Scoreboard players={props.engine.players()} scores={props.engine.gameState.scores} />
-              </Match>
-            </Switch>
           </div>
         </div>
       </Show>
