4d48aad2a58a24d1900f8b776c1efc52f2d53e47
George H <george@hoqqanen.com>
2023-01-19 16:33:43 -0800

    Styling improvements, change score prev calculation

--- dev/src/games/components/Scoreboard.tsx
+++ dev/src/games/components/Scoreboard.tsx
@@ -3,14 +3,32 @@ import { AvatarPlayer, Score } from '../engines/types';
 
 const Scoreboard: Component<{
   scores: Record<AvatarPlayer["id"], Score>,
-  players: AvatarPlayer[]
+  players: AvatarPlayer[],
 }> = (props) => {
+
+  // What do we want to show here?
+  // Each player's total score (done)
+  // Who each player fooled? And with what?
+
+  // It would be nice if the previous options stayed the same??
+  // Yes. Display all the captions, put who wrote each one on it, as well as who picked each
+  // Give the truth a green background.
+
+  // This scoreboard only renders the cumulative scores (and possibly shows the differences)
+
 	return (
     <>
       <h3>Scores:</h3>
-      <For each={props.players.sort((p1, p2) => props.scores[p2.id].current - props.scores[p1.id].current)}>{(p, i) => {
-        return <p><img src={p.avatar ?? ''} width="32" /> {props.scores[p.id].current}</p>
-      }}</For>
+      <div class="Scoreboard">
+        <For each={props.players.sort((p1, p2) => props.scores[p2.id].current - props.scores[p1.id].current)}>{(p, i) => {
+          return <div class="Scoreboard-Row">
+            {i()+1}: 
+            <img class="Scoreboard-Avatar" src={p.avatar ?? ''} /> 
+            {props.scores[p.id].current} 
+            {props.scores[p.id].current !== props.scores[p.id].previous ? ` (+${props.scores[p.id].current - props.scores[p.id].previous})` : ''}
+          </div>
+        }}</For>
+      </div>
     </>
 	)
 }
