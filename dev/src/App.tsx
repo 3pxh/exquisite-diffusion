d79ab401e7f34d7324753f0c17608f9631492ab9
George Hokkanen <hoqqanen@Georges-MacBook-Pro-2.local>
2023-01-26 21:16:32 -0800

    Link color

--- dev/src/App.tsx
+++ dev/src/App.tsx
@@ -99,7 +99,7 @@ const App: Component = () => {
             <JoinGame chooseGame={chooseGame} />
 
             <h3>Join us in making the fun!</h3>
-            <ul>
+            <ul class="SocialLinks">
               <li><a href="https://www.patreon.com/artifice_games" target="_blank">Contribute via Patreon</a></li>
               <li><a href="https://discord.gg/XwfUZTjS2p" target="_blank">Join the Discord</a></li>
               <li><a href="https://forms.gle/71FD149ktFhyYKT1A" target="_blank">Send feedback</a></li>
--- dev/src/GameSelection.tsx
+++ dev/src/GameSelection.tsx
@@ -134,7 +134,7 @@ const GameSelection: Component<{chooseGame: (g: GameType, roomId: number, shortc
         }}</For>
         <div class="GameSelection-LeftFooter">
           Join us in making the fun!
-          <ul>
+          <ul class="SocialLinks">
             <li><a href="https://www.patreon.com/artifice_games" target="_blank">Contribute via Patreon</a></li>
             <li><a href="https://discord.gg/XwfUZTjS2p" target="_blank">Join the Discord</a></li>
             <li><a href="https://forms.gle/71FD149ktFhyYKT1A" target="_blank">Send feedback</a></li>
